db94548c9d8d9834c8da5d66d074d3424e63fbf6
101arrowz <arjunbarrett@gmail.com>
2019-06-24 23:06:04 -0700

    Updated

--- mlutils.py
+++ mlutils.py
@@ -1,98 +1,230 @@
 import pandas as pd
 import numpy as np
 import warnings
-def load(d, literal_data=False, encoding=None, delim=',', dtype=np.float32, header=True, class_col=-1, replace={}, random_seed=np.random.randint(65535), test_split=0.2):
-    """
-    Returns a tuple (X_train, X_test, y_train, y_test) given a dataset.
+import os
+from hashlib import md5
+import pickle
+class DataLoader:
+    def __init__(self, d, usesave=False, save=True, literal_data=False, encoding=None, delim=',', dtype=np.float32, header=True, class_col=-1, remove=None, fill_to_max=None, replace={}, random_seed=np.random.randint(65535), test_split=0.2):
+        """
+        A data loader.
 
-    `d`
-    A filepath, URL, or raw string to load data from.
+        `d`
+        A filepath, URL, or raw string to load data from.
 
-    `literal_data`
-    Whether `d` should be interpreted literally. Optional, but avoids a warning.
+        `usesave`
+        Whether to use a pickled version of the current dataset. Make "True" when finished data manipulation.
 
-    `encoding`
-    The data's encoding. The default of `None` will automatically pick the best encoding.
+        `save`
+        Whether to pickle and save the current dataset.
 
-    `delim`
-    The delimiter to be passed to `pandas.read_csv`.
+        `literal_data`
+        Whether `d` should be interpreted literally. Optional, but avoids a warning.
 
-    `dtype`
-    The datatype of the numpy arrays. Set to `None` to let `pandas` decide or have different datatypes for the labels and data.
+        `encoding`
+        The data's encoding. The default of `None` will automatically pick the best encoding.
 
+        `delim`
+        The delimiter to be passed to `pandas.read_csv`.
 
-    """
-    from io import StringIO
-    def loadLiteral(d, encoding):
-        try:
-            if encoding:
-                raw = d.decode()
-            else:
-                raw = d
-        except AttributeError:
-            raise ValueError("cannot have encoding argument when passing data as an unencoded string") from None
-        return raw
-    if not isinstance(d, (str, bytes)):
-        raise TypeError("d must be of type string (or bytes when passing raw, encoded data)")
-    if not isinstance(literal_data, bool):
-        raise TypeError("literal_data must be of type string or bytes (when passing raw, encoded data)")
-    if not isinstance(encoding, str) and encoding is not None:
-        raise TypeError('encoding must be of type string or None')
-    if not isinstance(header, bool):
-        raise TypeError('header must be of type bool')
-    if isinstance(class_col, (int, str)):
-        class_col = (class_col,)
-    strindices = False
-    try:
-        if not all(map(lambda v: isinstance(v, int), class_col)):
-            if not all(map(lambda v: isinstance(v, str), class_col)):
-                raise TypeError('class_col must contain only int values or only str values')
-            else:
-                strindices = True
-                if not header:
-                    raise TypeError("a header must exist to access class_col with str indices")
-    except TypeError as e:
-        raise (TypeError('class_col must be an int or an iterable') if str(e)[0] == "'" else e) from None
-    if not isinstance(replace, dict):
-        raise TypeError('replace must be a dict with the keys being the strings for replacement and the values being the new strings')
-    if not all(isinstance(v, str) and isinstance(k, str) for k,v in replace.items()):
-        raise TypeError('every key and value in replace must be a string')
-    header = 0 if header else None
-    if literal_data:
-        raw = loadLiteral(d, encoding)
-    else:
-        try:
-            with open(d, encoding=encoding) as f:
-                raw = f.read()
-        except (FileNotFoundError, OSError):
+        `dtype`
+        The datatype of the numpy arrays. Set to `None` to let `pandas` decide or have different datatypes for the labels and data.
+
+        `header`
+        Whether there is a header containing labels for each column
+        
+        `class-col`
+        An index/iterable of indices or name/iterable of names for the column(s) containing labels.
+
+        `remove`
+        An index/iterable of indices or name/iterable of names for the column(s) to be outright removed.
+        
+        `replace`
+        A dictionary with keys (to replace) and values (what to replace with)
+
+        `seed`
+        The random seed. Use to remove randomness.
+
+        `test_split`
+        The decimal out of one to use as testing data.
+        """
+
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        fulldata = None
+        cachedir = '__pycache__'
+        if not os.path.isdir(cachedir):
+            if os.path.exists(cachedir):
+                os.remove(cachedir)
+            os.makedirs(cachedir)
+        if not isinstance(d, (str, bytes)):
+            raise Exception('p must be of type string (or bytes when passing raw, encoded data)')
+        fn = md5(d if isinstance(d, bytes) else d.encode('UTF-8')).hexdigest()
+        datafp = os.path.join(cachedir, fn+'.pkldata')
+        if usesave:
             try:
-                import requests
-                r = requests.get(d)
-                r.encoding = encoding if encoding else r.apparent_encoding
-                raw = r.text
-            except:
-                warnings.warn("interpreting d as a literal string of data. This is usually not the best solution. To disable this warning, set literal_data to True.", RuntimeWarning)
-                raw = loadLiteral(d, encoding)
-    if not raw:
-        raise ValueError("input file or string contained no data")
-    df = pd.read_csv(StringIO(raw), sep=delim, header=header)
-    for k, v in replace.items(): df.replace(k, v, inplace=True)
-    if strindices:
-        labels = df[[*class_col]]
-        df.drop([*class_col], axis=1, inplace=True)
-    else:
-        labels = pd.concat([df.iloc[:, col] for col in class_col], axis=1)
-        df.drop(df.columns[[*class_col]], axis=1, inplace=True)
-    try:
-        data = df.to_numpy(dtype=dtype)
-        labels = np.squeeze(labels.to_numpy(dtype=dtype))
-    except TypeError:
-        raise TypeError('invalid dtype: {} is not a legal datatype for a numpy array'.format(dtype)) from None
-    np.random.seed(random_seed); np.random.shuffle(data)
-    np.random.seed(random_seed); np.random.shuffle(labels)
-    lim = int(len(labels)*test_split)
-    if not lim:
-        raise ValueError("no testing samples. This could be due to improper loading of the data (for example, incorrect delimiter).")
-    return (data[:-lim], data[-lim:], labels[:-lim], labels[-lim:])
-if __name__ == "__main__":
-    print(load('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', dtype=None))
+                with open(datafp, 'rb') as f:
+                    fulldata = pickle.load(f)
+            except FileNotFoundError:
+                print('Failed to find saved file! Loading directly...')
+        if not fulldata: fulldata = self.__class__.load(**locals())
+        if save:
+            try:
+                with open(datafp, 'wb') as f:
+                    pickle.dump(fulldata, f, protocol=pickle.HIGHEST_PROTOCOL)
+            except Exception as e:
+                print('Could not save data for an unknown reason. See the following exception:', e)
+        self.data = fulldata
+        self.X_train, self.X_test, self.y_train, self.y_test = self.data
+    @staticmethod
+    def load(d, literal_data=False, encoding=None, delim=',', dtype=np.float32, header=True, class_col=-1, remove=None, fill_to_max=None, replace={}, random_seed=np.random.randint(65535), test_split=0.2, **kwargs):
+        """
+        Returns a tuple (X_train, X_test, y_train, y_test) given a dataset.
+
+        `d`
+        A filepath, URL, or raw string to load data from.
+
+        `literal_data`
+        Whether `d` should be interpreted literally. Optional, but avoids a warning.
+
+        `encoding`
+        The data's encoding. The default of `None` will automatically pick the best encoding.
+
+        `delim`
+        The delimiter to be passed to `pandas.read_csv`.
+
+        `dtype`
+        The datatype of the numpy arrays. Set to `None` to let `pandas` decide or have different datatypes for the labels and data.
+
+        `header`
+        Whether there is a header containing labels for each column
+        
+        `class-col`
+        An index/iterable of indices or name/iterable of names for the column(s) containing labels.
+
+        `remove`
+        An index/iterable of indices or name/iterable of names for the column(s) to be outright removed.
+
+        `replace`
+        A dictionary with keys (to replace) and values (what to replace with)
+
+        `seed`
+        The random seed. Use to remove randomness.
+
+        `test_split`
+        The decimal out of one to use as testing data.
+        """
+        from io import StringIO
+        def loadLiteral(d, encoding):
+            raw = None
+            try:
+                if encoding:
+                    raw = d.decode()
+                else:
+                    raw = d
+            except AttributeError:
+                raise ValueError("cannot have encoding argument when passing data as an unencoded string") from None
+            return raw
+        if not isinstance(d, (str, bytes)):
+            raise TypeError("d must be of type string (or bytes when passing raw, encoded data)")
+        if not isinstance(literal_data, bool):
+            raise TypeError("literal_data must be of type string or bytes (when passing raw, encoded data)")
+        if not isinstance(encoding, str) and encoding is not None:
+            raise TypeError('encoding must be of type string or None')
+        if not isinstance(header, bool):
+            raise TypeError('header must be of type bool')
+        if isinstance(class_col, (int, str)):
+            class_col = (class_col,)
+        strindices = False
+        try:
+            if not all(map(lambda v: isinstance(v, int), class_col)):
+                if not all(map(lambda v: isinstance(v, str), class_col)):
+                    raise TypeError('class_col must contain only int values or only str values')
+                else:
+                    strindices = True
+                    if not header:
+                        raise TypeError("a header must exist to access class_col with str indices")
+        except TypeError as e:
+            raise (TypeError('class_col must be an int or an iterable') if str(e)[0] == "'" else e) from None
+        if isinstance(remove, (int, str)):
+            remove = (remove,)
+        strindices = False
+        if remove is not None:
+            try:
+                if not all(map(lambda v: isinstance(v, int), class_col)):
+                    if not all(map(lambda v: isinstance(v, str), class_col)):
+                        raise TypeError('remove must contain only int values or only str values')
+                    else:
+                        strindices = True
+                        if not header:
+                            raise TypeError("a header must exist to access remove with str indices")
+            except TypeError as e:
+                raise (TypeError('remove must be an int or an iterable') if str(e)[0] == "'" else e) from None
+        if not isinstance(fill_to_max, bool):
+            if fill_to_max is not None:
+                raise TypeError('fill_to_max must be a boolean or None (if no replication of terms is desired)')
+        elif len(class_col) < 1:
+            raise ValueError('cannot have multiple class columns if filling. Set fill_to_max to None to remove this error.')
+        if not isinstance(replace, dict):
+            raise TypeError('replace must be a dict with the keys being the strings for replacement and the values being the new strings')
+        if not all(isinstance(v, str) and isinstance(k, str) for k,v in replace.items()):
+            raise TypeError('every key and value in replace must be a string')
+        if not isinstance(random_seed, int):
+            raise TypeError('random_seed must be of type int')
+        if not isinstance(test_split, float):
+            raise TypeError('test_split must be of type float')
+        if not (0 < test_split < 1):
+            raise ValueError('test_split must be greater than 0 but less than 1')
+        header = 0 if header else None
+        if literal_data:
+            raw = loadLiteral(d, encoding)
+        else:
+            try:
+                with open(d, encoding=encoding) as f:
+                    raw = f.read()
+            except (FileNotFoundError, OSError):
+                try:
+                    import requests
+                    r = requests.get(d)
+                    r.encoding = encoding if encoding else r.apparent_encoding
+                    raw = r.text
+                except:
+                    warnings.warn("interpreting d as a literal string of data. This is usually not the best solution. To disable this warning, set literal_data to True.", RuntimeWarning)
+                    raw = loadLiteral(d, encoding)
+        if not raw:
+            raise ValueError("input file or string contained no data")
+        df = pd.read_csv(StringIO(raw), sep=delim, header=header)
+        for k, v in replace.items(): df.replace(k, v, inplace=True)
+        if fill_to_max is not None:
+            labels = df[class_col[0]] if strindices else df.iloc[:, class_col[0]]
+            valcs = labels.value_counts()
+            if fill_to_max:
+                maxc = max(valcs)
+                for c in labels.unique():
+                    subset = df[labels == c]
+                    a = valcs[c]
+                    diff = maxc-a
+                    for i in range(diff//a):
+                        df = df.append(subset, ignore_index=True)
+                    if diff % a:
+                        np.random.seed(random_seed); df = df.append(subset.iloc[np.random.choice(len(subset), diff % a, replace=False)])
+            else:
+                raise NotImplementedError("fill_to_max=False is not supported. Use None for no filling or removal, or True for filling.")
+        if strindices:
+            labels = df[class_col[0]]
+            df.drop([*class_col], axis=1, inplace=True)
+            if remove is not None: df.drop([*remove], axis=1, inplace=True)
+        else:
+            labels = df.iloc[:, class_col[0]]
+            df.drop(df.columns[[*class_col]], axis=1, inplace=True)
+            if remove is not None: df.drop(df.columns[[*remove]], axis=1, inplace=True)
+        try:
+            data = df.to_numpy(dtype=dtype)
+            labels = np.squeeze(labels.to_numpy(dtype=dtype))
+        except TypeError:
+            raise TypeError('invalid dtype: {} is not a legal datatype for a numpy array'.format(dtype)) from None
+        np.random.seed(random_seed); np.random.shuffle(data)
+        np.random.seed(random_seed); np.random.shuffle(labels)
+        lim = int(len(labels)*test_split)
+        if not lim:
+            raise ValueError("no testing samples. This could be due to improper loading of the data (for example, incorrect delimiter).")
+        return (data[:-lim], data[-lim:], labels[:-lim], labels[-lim:])
